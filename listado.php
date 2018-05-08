2c5d64857a808ee39c4e0a844a2cdc3d7f884f27
AngelAranda <aacursoforem@gmail.com>
2018-05-08 13:36:30 +0200

    Version 3 listado
    Se usa bootstrap para mostrar la tabla con los datos del listado

--- listado.php
+++ listado.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Listado de tienda</title>
-<!-- 	<meta charset="utf-8" />  -->
+	<title>Listado de Productos</title>
+	<meta charset="utf-8" /> 
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+			
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">	
 </head> 
+
 <body>
+	<div class="jumbotron text-center">
+			<h1>Listado de producto</h1>
+	</div>
+	<div class="container">
+	<table class="table table-striped text-center">
+	<thead class="thead-dark">
+		<tr>
+			<th>Código</th>
+			<th>Nombre</th>
+			<th>Categoría</th>
+			<th>Cantidad</th>
+			<th>Precio</th>
+			<th>Modificar</th>
+			<th>Eliminar</th>
+		</tr>
+	<thead>
+	<tbody>
 <?php
 
 	// Conectamos a la base de datos
@@ -18,42 +43,21 @@
 	// Recorremos el resultSet para ir extrayendo/mostrando los resultados devueltos
 	// Vamos guardando cada una de las filas del resultSet en la variable $reg 
 	while ( $reg = mysqli_fetch_array($registros) ) {
-		echo "Código: ".$reg['codigo']. "<br>";
-		echo "Nombre: ".$reg['nombre']. "<br>";
-		echo "Categoría: ".$reg['categoria']. "<br>";
-		echo "Cantidad: ".$reg['cantidad']. "<br>";
-		echo "Precio: ".$reg['precio']. "<br>";
-		echo "<hr>";
+		echo '<tr>';
+		echo '<th>'.$reg['codigo'].'</th>';
+		echo '<td>'.$reg['nombre'].'</td>';
+		echo '<td>'.$reg['categoria'].'</td>';
+		echo '<td class="text-right">'.$reg['cantidad'].'</td>';
+		echo '<td class="text-right">'.$reg['precio'].'</td>';
+		echo'<td><a href="#" class=""><i class="fas fa-edit fa-2x"></i></a> </td>';
+		echo'<td><a href="#" class=""><i class="fas fa-trash-alt fa-2x"></i></a> </td>';
+		echo'</tr>';
 	}
-
-
-
 
 ?>
 
-
-<h3>Tabla para ponerlo bonito en bootstrap</h3>
-<table class="">
-<thead>
-<tr>
-	<th>Código<th>
-	<th>Nombre</th>
-	<th>Categoría</th>
-	<th>Cantidad</th>
-	<th>Precio</th>
-</tr>
-<thead>
-<tbody>
-
-<tr>
-	<td>$codigo</th>
-	<td>$nombre</td>
-	<td>$categoria</td>
-	<td>$cantidad</td>
-	<td>$precio</td>
-</tr>
-
-</tbody>
-</table>
+	</tbody>
+	</table>
+	</div>
 </body>
 </html>
