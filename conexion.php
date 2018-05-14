5008b65805cb16c8f36434250554240bf84e69f0
Angel Aranda <38736519+aacursoforem@users.noreply.github.com>
2018-05-14 23:39:27 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 	// Conectamos a la base de datos
-	$conexion = mysqli_connect("localhost", "usuario05", "aula05", "cf_tienda") or die ("Error al conectar a la base de datos");
+	$conexion = mysqli_connect("localhost", "root", "", "tienda") or die ("Error al conectar a la base de datos");
 
 
 ?>
